edf9dfdfd8900fcbc028daa9293c17b3f1a316f5
Bharat Mundra <mbharat@proton.me>
2026-06-28 21:00:12 +0530

    straight target only

--- core/target_manager.py
+++ core/target_manager.py
@@ -20,8 +20,18 @@ class TargetManager:
         self.color = random_color()
 
     def spawn(self, width, height):
-        self.x = random.randint(150, width-150)
-        self.y = random.randint(int(height * 0.4), int(height * 0.7))
+        center_x = width // 2
+
+        #horizontal zone: only center area
+        min_x = center_x - 150
+        max_x = center_x + 150
+
+        #vertical zone: chest to head height
+        min_y = int(height * 0.35)
+        max_y = int(height * 0.55)
+
+        self.x = random.randint(min_x, max_x)
+        self.y = random.randint(min_y, max_y)
         self.color = random_color() #new random color everytime
         self.last_spawn = time.time()
 
