a9cafa609e8c766d07fff0a2dba0426af398540d
Bharat Mundra <mbharat@proton.me>
2026-06-28 20:55:51 +0530

    fix speed detection of player

--- core/punch_detector.py
+++ core/punch_detector.py
@@ -2,35 +2,59 @@ import numpy as np
 import time
 
 class PunchDetector:
-    def __init__(self, speed_threshold=25):
+    def __init__(self, speed_threshold=40, margin_factor=2.0):
         self.prev_right = None
         self.prev_left = None
         self.prev_time = time.time()
+        
         self.speed_threshold = speed_threshold
+        #hit zone ke bahr se andr aane ki condition
+        self.margin_factor = margin_factor # radius * factor = outer zone
 
     def distance(self, p1, p2):
         return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
     
-    def get_speed(self, prev, current, dt):
-        if prev is None:
-            return 0
-        return self.distance(prev, current)/ dt
-    
     def detect_hit(self, right, left, target, radius):
         now = time.time()
         dt = now - self.prev_time
+        if dt == 0:
+            dt = 0.001
 
         hit = False
+        outer_radius = radius * self.margin_factor 
 
-        if right:
-            speed_r = self.get_speed(self.prev_right, right, dt)
-            if speed_r > self.speed_threshold and self.distance(right, target) < radius:
-                hit = True
+        def check_punch(prev, curr):
+            if prev is None or curr is None:
+                return False
+            
+            #speed
+            speed = self.distance(prev,curr) / dt
+            if speed < self.speed_threshold:
+                return False
+            
+            #distances
+            prev_dist = self.distance(prev, target)
+            curr_dist = self.distance(curr, target)
 
-        if left:
-            speed_l = self.get_speed(self.prev_left, left, dt)
-            if speed_l > self.speed_threshold and self.distance(left, target) < radius:
-                hit = True
+            if curr_dist > prev_dist:
+                return False
+
+            #condition
+            # 1. phle outer zone k bahr tha
+            # 2. ab inner radius k andr aa gya
+            # 3. movement target k trf h
+            if prev_dist > outer_radius and curr_dist < radius:
+                return True
+            
+            return False
+
+        #right hand
+        if right and check_punch(self.prev_right, right):
+            hit = True
+
+        #left hand
+        if left and check_punch(self.prev_left, left):
+            hit = True
 
         self.prev_right = right
         self.prev_left = left
